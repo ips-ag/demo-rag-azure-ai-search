5978091bdce12da1b1beb660701f0b015c51dd66
Uroš Miletić <uros.miletic@ips-ag.com>
2024-01-22 15:50:56 +0100

    Add default React frontend (#4)
    * Move UI project to ASP.NET web app
    Configure SPA services and middleware
    
    * Replace create-react-app with vite
    
    * Move to app directory

---
+++ src/app/Program.cs
@@ -0,0 +1,46 @@
+using Api.Azure.OpenAi.Extensions;
+using Api.Azure.Search.Extensions;
+using Api.Extensions.Endpoints;
+using Api.Extensions.ExceptionHandler;
+using Api.Features.Extensions;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => options.CustomSchemaIds(type => type.FullName));
+builder.Services.AddProblemDetailsExceptionHandler();
+builder.Services.AddSpaStaticFiles(configuration => configuration.RootPath = "ClientApp/dist");
+
+builder.Services.AddFeatures();
+builder.Services.AddOpenAi().AddAiSearch();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseExceptionHandler();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapFeatureEndpoints();
+
+app.UseSpaStaticFiles();
+
+app.UseSpa(
+    spa =>
+    {
+        spa.Options.SourcePath = "ClientApp";
+        spa.Options.DevServerPort = 5173;
+        spa.UseReactDevelopmentServer(npmScript: "dev");
+    });
+
+app.Run();
