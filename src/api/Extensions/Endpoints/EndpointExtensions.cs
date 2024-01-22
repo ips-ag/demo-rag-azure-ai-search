69d9dc1d475cff621fb8fcf5490f2437d0dc0dfa
Uroš Miletić <uros.miletic_c@sandvik.com>
2024-01-22 15:35:44 +0100

    Replace create-react-app with vite

---
+++ src/api/Extensions/Endpoints/EndpointExtensions.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using RagCommands = Api.Features.Rag.Commands;
+using RagModels = Api.Features.Rag.Models;
+using HybridRagCommands = Api.Features.HybridRag.Commands;
+using HybridRagModels = Api.Features.HybridRag.Models;
+
+namespace Api.Extensions.Endpoints
+{
+    internal static class EndpointExtensions
+    {
+        public static IEndpointRouteBuilder MapFeatureEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapPost(
+                    "/rag",
+                    ([FromBody] RagModels.SearchRequest request, [FromServices] IMediator mediator) =>
+                    {
+                        var command = new RagCommands.SearchCommand(request);
+                        return mediator.Send(command);
+                    })
+                .Produces<RagModels.SearchResponse>()
+                .ProducesValidationProblem()
+                .WithName("GetResultUsingRag")
+                .WithOpenApi();
+
+            app.MapPost(
+                    "/hybridrag",
+                    ([FromBody] HybridRagModels.SearchRequest request, [FromServices] IMediator mediator) =>
+                    {
+                        var command = new HybridRagCommands.SearchCommand(request);
+                        return mediator.Send(command);
+                    })
+                .Produces<HybridRagModels.SearchResponse>()
+                .ProducesValidationProblem()
+                .WithName("GetResultUsingHybridRag")
+                .WithOpenApi();
+
+            return app;
+        }
+    }
+}
--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,21 +1,16 @@
 using Api.Azure.OpenAi.Extensions;
 using Api.Azure.Search.Extensions;
+using Api.Extensions.Endpoints;
 using Api.Extensions.ExceptionHandler;
 using Api.Features.Extensions;
-using MediatR;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
-using RagCommands = Api.Features.Rag.Commands;
-using RagModels = Api.Features.Rag.Models;
-using HybridRagCommands = Api.Features.HybridRag.Commands;
-using HybridRagModels = Api.Features.HybridRag.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options => options.CustomSchemaIds((type) => type.FullName));
+builder.Services.AddSwaggerGen(options => options.CustomSchemaIds(type => type.FullName));
 builder.Services.AddProblemDetailsExceptionHandler();
 builder.Services.AddSpaStaticFiles(configuration => configuration.RootPath = "ClientApp/dist");
 
@@ -36,36 +31,16 @@ app.UseExceptionHandler();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-app.MapPost(
-        "/rag",
-        ([FromBody] RagModels.SearchRequest request, [FromServices] IMediator mediator) =>
-        {
-            var command = new RagCommands.SearchCommand(request);
-            return mediator.Send(command);
-        })
-    .Produces<RagModels.SearchResponse>()
-    .ProducesValidationProblem()
-    .WithName("GetResultUsingRag")
-    .WithOpenApi();
-
-app.MapPost(
-        "/hybridrag",
-        ([FromBody] HybridRagModels.SearchRequest request, [FromServices] IMediator mediator) =>
-        {
-            var command = new HybridRagCommands.SearchCommand(request);
-            return mediator.Send(command);
-        })
-    .Produces<HybridRagModels.SearchResponse>()
-    .ProducesValidationProblem()
-    .WithName("GetResultUsingHybridRag")
-    .WithOpenApi();
+app.MapFeatureEndpoints();
 
 app.UseSpaStaticFiles();
 
-app.UseSpa(spa =>
-{
-    spa.Options.SourcePath = "ClientApp";
-    spa.UseReactDevelopmentServer(npmScript: "start");
-});
+app.UseSpa(
+    spa =>
+    {
+        spa.Options.SourcePath = "ClientApp";
+        spa.Options.DevServerPort = 5173;
+        spa.UseReactDevelopmentServer(npmScript: "dev");
+    });
 
 app.Run();
